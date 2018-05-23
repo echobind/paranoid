1bf8cdd0b04563770f98ce67f6e4bf404176c8a8
Robert Beene <robert@echobind.com>
2018-05-22 21:17:15 -0400

    refactor

--- lib/paranoid/ecto.ex
+++ lib/paranoid/ecto.ex
@@ -11,58 +11,47 @@ defmodule Paranoid.Ecto do
     if repo = Keyword.get(opts, :repo) do
       quote do
         def all(queryable, opts \\ []) do
-          unquote(repo)
-          |> apply(:all, [update_queryable(queryable, opts), opts])
+          unquote(repo).all(update_queryable(queryable, opts), opts)
         end
 
         def stream(queryable, opts \\ []) do
-          unquote(repo)
-          |> apply(:stream, [update_queryable(queryable, opts), opts])
+          unquote(repo).stream(update_queryable(queryable, opts), opts)
         end
 
         def get(queryable, id, opts \\ []) do
-          unquote(repo)
-          |> apply(:get, [update_queryable(queryable, opts), id, opts])
+          unquote(repo).get(update_queryable(queryable, opts), id, opts)
         end
 
         def get!(queryable, id, opts \\ []) do
-          unquote(repo)
-          |> apply(:get!, [update_queryable(queryable, opts), id, opts])
+          unquote(repo).get!(update_queryable(queryable, opts), id, opts)
         end
 
         def get_by(queryable, clauses, opts \\ []) do
-          unquote(repo)
-          |> apply(:get_by, [update_queryable(queryable, opts), clauses, opts])
+          unquote(repo).get_by(update_queryable(queryable, opts), clauses, opts)
         end
 
         def get_by!(queryable, clauses, opts \\ []) do
-          unquote(repo)
-          |> apply(:get_by!, [update_queryable(queryable, opts), clauses, opts])
+          unquote(repo).get_by!(update_queryable(queryable, opts), clauses, opts)
         end
 
         def one(queryable, opts \\ []) do
-          unquote(repo)
-          |> apply(:one, [update_queryable(queryable, opts), opts])
+          unquote(repo).one(update_queryable(queryable, opts), opts)
         end
 
         def one!(queryable, opts \\ []) do
-          unquote(repo)
-          |> apply(:one!, [update_queryable(queryable, opts), opts])
+          unquote(repo).one!(update_queryable(queryable, opts), opts)
         end
 
         def aggregate(queryable, aggregate, field, opts \\ []) do
-          unquote(repo)
-          |> apply(:aggregate, [update_queryable(queryable, opts), aggregate, field, opts])
+          unquote(repo).aggregate(update_queryable(queryable, opts), aggregate, field, opts)
         end
 
         def insert_all(schema_or_source, entries, opts \\ []) do
-          unquote(repo)
-          |> apply(:insert_all, [schema_or_source, entries, opts])
+          unquote(repo).insert_all(schema_or_source, entries, opts)
         end
 
         def update_all(queryable, updates, opts \\ []) do
-          unquote(repo)
-          |> apply(:update_all, [update_queryable(queryable, opts), updates, opts])
+          unquote(repo).update_all(update_queryable(queryable, opts), updates, opts)
         end
 
         def delete_all(queryable, opts \\ []) do
@@ -70,43 +59,35 @@ defmodule Paranoid.Ecto do
         end
 
         def delete_all(queryable, opts, _has_deleted_column = true) do
-          unquote(repo)
-          |> apply(:update_all, [update_queryable(queryable, opts), [set: [deleted_at: DateTime.utc_now()]]])
+          unquote(repo).update_all(update_queryable(queryable, opts), [set: [deleted_at: DateTime.utc_now()]])
         end
 
         def delete_all(queryable, opts, _no_deleted_column) do
-          unquote(repo)
-          |> apply(:delete_all, [queryable, opts])
+          unquote(repo).delete_all(queryable, opts)
         end
 
         def insert(struct, opts \\ []) do
-          unquote(repo)
-          |> apply(:insert, [struct, opts])
+          unquote(repo).insert(struct, opts)
         end
 
         def insert!(struct, opts \\ []) do
-          unquote(repo)
-          |> apply(:insert!, [struct, opts])
+          unquote(repo).insert!(struct, opts)
         end
 
         def update(struct, opts \\ []) do
-          unquote(repo)
-          |> apply(:update, [struct, opts])
+          unquote(repo).update(struct, opts)
         end
 
         def update!(struct, opts \\ []) do
-          unquote(repo)
-          |> apply(:update!, [struct, opts])
+          unquote(repo).update!(struct, opts)
         end
 
         def insert_or_update(changeset, opts \\ []) do
-          unquote(repo)
-          |> apply(:insert_or_update, [changeset, opts])
+          unquote(repo).insert_or_update(changeset, opts)
         end
 
         def insert_or_update!(changeset, opts \\ []) do
-          unquote(repo)
-          |> apply(:insert_or_update!, [changeset, opts])
+          unquote(repo).insert_or_update!(changeset, opts)
         end
 
         def delete(struct, opts \\ []) do
@@ -114,13 +95,11 @@ defmodule Paranoid.Ecto do
         end
 
         def delete(struct, opts, _has_deleted_column = true) do
-          unquote(repo)
-          |> apply(:update, [delete_changeset(struct), opts])
+          unquote(repo).update(delete_changeset(struct), opts)
         end
 
         def delete(struct, opts, _no_deleted_column = false) do
-          unquote(repo)
-          |> apply(:delete, [struct, opts])
+          unquote(repo).delete(struct, opts)
         end
 
         def delete!(struct, opts \\ []) do
@@ -128,18 +107,15 @@ defmodule Paranoid.Ecto do
         end
 
         def delete!(struct, opts, _has_deleted_column = true) do
-          unquote(repo)
-          |> apply(:update!, [delete_changeset(struct), opts])
+          unquote(repo).update!(delete_changeset(struct), opts)
         end
 
         def delete!(struct, opts, _no_deleted_column = false) do
-          unquote(repo)
-          |> apply(:delete!, [struct, opts])
+          unquote(repo).delete!(struct, opts)
         end
 
         def undelete!(struct, opts \\ []) do
-          unquote(repo)
-          |> apply(:update!, [undelete_changeset(struct), opts])
+          unquote(repo).update!(undelete_changeset(struct), opts)
         end
 
         defp delete_changeset(struct) do
@@ -151,13 +127,11 @@ defmodule Paranoid.Ecto do
         end
 
         def preload(struct_or_structs_or_nil, preloads, opts \\ []) do
-          unquote(repo)
-          |> apply(:preload, [struct_or_structs_or_nil, preloads, opts])
+          unquote(repo).preload(struct_or_structs_or_nil, preloads, opts)
         end
 
         def load(schema_or_types, data) do
-          unquote(repo)
-          |> apply(:load, [schema_or_types, data])
+          unquote(repo).load(schema_or_types, data)
         end
 
         defp update_queryable(queryable, opts) when is_list(opts) do
